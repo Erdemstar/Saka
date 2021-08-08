c7e95a73f638561264ed2d510216a07436017cc1
Erdemstar <erdem-yildiz@windowslive.com>
2021-08-08 14:50:33 +0300

    Created reflected-xss-js-variable names project. It take username from querystring and pass onto it javascript

---
+++ reflected-xss-js-variable/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using reflected_xss_js_variable.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_js_variable.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Attack()
+        {
+            Models.Username username = new Models.Username() { Name = "" };
+
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["Name"]))
+            {
+                username.Name = HttpContext.Request.Query["Name"];
+            }
+            return View(username);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ reflected-xss-js-variable/Models/Username.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_js_variable.Models
+{
+    public class Username
+    {
+        public string Name { get; set; }
+    }
+}

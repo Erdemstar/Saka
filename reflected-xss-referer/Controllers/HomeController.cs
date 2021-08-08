1628eb66f27fcf8b505e888e408451455cec6125
Erdemstar <erdem-yildiz@windowslive.com>
2021-08-08 15:16:16 +0300

    Created reflected-xss-referer named project. It read referer header and pass it hidden input at attack page

---
+++ reflected-xss-referer/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using reflected_xss_referer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_referer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Attack()
+        {
+            string referer = Request.Headers["Referer"].ToString();
+            ViewBag.referer = referer;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Attack(string temporay)
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ reflected-xss-referer/Models/Username.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_referer.Models
+{
+    public class Username
+    {
+        public string username { get; set; }
+    }
+}

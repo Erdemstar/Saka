129df2e585dfc0c82258f8a3ff165d1e7fcf01e5
Erdemstar <erdem-yildiz@windowslive.com>
2021-08-08 16:06:45 +0300

    Created reflected-xss-tag-attribute named project. It take url from user and pass it img src part.

---
+++ reflected-xss-tag-attribute/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using reflected_xss_tag_attribute.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_tag_attribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Attack()
+        {
+            Models.Image image = new Models.Image() { url = ""};
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["url"])) {
+                image.url = HttpContext.Request.Query["url"];
+            }
+            
+            return View(image);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ reflected-xss-tag-attribute/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_tag_attribute.Models
+{
+    public class Image
+    {
+        public string url { get; set; }
+    }
+}

0392e1e76a955845f0a648de0ddc9acc69d2120e
Erdemstar <erdem-yildiz@windowslive.com>
2021-08-08 16:30:39 +0300

    Created reflected-xss-user-agent named project. It take user-agent from request and pass it Attack page

---
+++ reflected-xss-user-agent/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using reflected_xss_user_agent.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reflected_xss_user_agent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Attack()
+        {
+            ViewBag.userAgent = "";
+            if (!string.IsNullOrEmpty(HttpContext.Request.Headers["User-agent"])) {
+
+                ViewBag.userAgent = HttpContext.Request.Headers["User-agent"];
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ reflected-xss-user-agent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reflected_xss_user_agent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
